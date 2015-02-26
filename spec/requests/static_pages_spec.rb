0aab67c041f23234dbd70d9bec63f3da87198741
Dima <Gunosada1@gmail.com>
2015-02-26 16:54:31 +0200

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,25 +4,36 @@ describe "StaticPages" do
 
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
-			visit'/static_pages/home'
-			expect(page).to have_content('Sample App')
+	   		visit '/static_pages/home'
+	    	expect(page).to have_content('Sample App')
 		end
 
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on rails Sample App | Home")
-		end
-	end
+	    it "should have the base title" do
+	    	visit '/static_pages/home'
+	    	expect(page).to have_title("Ruby on rails Sample App")
+	    end
+
+	    it "should not have a custom page title" do
+	       visit '/static_pages/home'
+	       expect(page).not_to have_title('| Home')
+	    end
+	end 
+	
 
 	describe "Help page" do
 		it "Should have the content 'help'" do
 			visit '/static_pages/help'
-			expect(page).to have_content('Help')
+			expect(page).to have_content('Sample App')
 		end
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on rails Sample App | Help")
+			expect(page).to have_title("Ruby on rails Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit "/static_pages/help"
+			expect(page).not_to have_title('| help')
 		end
 	end
 
@@ -34,7 +45,12 @@ describe "StaticPages" do
 
 		it "should have the title 'About'" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on rails Sample App | About")
+			expect(page).to have_title("Ruby on rails Sample App")
+		end
+
+		it "" do
+			visit"/static_pages/about"
+			expect(page).not_to have_title('| about')
 		end
 	end
 
@@ -46,8 +62,13 @@ describe "StaticPages" do
 
 		it "should have the title 'Contact'" do
 			visit '/static_pages/contact'
-			expect(page).to have_title("Ruby on rails Sample App | Contact")
+			expect(page).to have_title("Ruby on rails Sample App")
 		end
 	end
+		it "---/----" do
+			visit '/static_pages/contact'
+			expect(page).not_to have_title('| contact')
+		end
+
 
 end	
